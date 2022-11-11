henniaufmrenni/boum

commit eddc9359e0a618edaebc2859e813e3f22c49f2e0

Fix: like action on AlbumScreen

app/components/ListItems.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import {
   Dimensions,
   StyleSheet,
@@ -69,8 +69,11 @@ const ListHeader = ({
   selectedStorageLocation,
 }: ListHeaderProps): JSX.Element => {
   const jellyfin = new jellyfinClient();
-  console.log('Selected Storage Header', selectedStorageLocation);
 
+  const [isFavorite, setIsFavorite] = useState<boolean>(false);
+  useEffect(() => {
+    setIsFavorite(item.UserData.IsFavorite);
+  }, []);
   return (
     <>
       {averageColorRgb && item ? (
@@ -177,20 +180,17 @@ const ListHeader = ({
                       <Icon name="ios-list" size={30} color={colours.accent} />
                     </Text>
                   </TouchableOpacity>
-                  {'UserData' in item !== false && item.UserData.IsFavorite ? (
+                  {isFavorite ? (
                     <TouchableOpacity
-                      onPress={async () =>
+                      onPress={async () => {
                         await jellyfin
-                          .postFavorite(session, item.Id, 'POST')
-                          .then(res => {
-                            mutate();
-                            console.log('Success posting favourites: ', res);
-                          })
-                          .catch(res => {
-                            mutate();
-                            console.log('Error posting favourites: ', res);
-                          })
-                      }
+                          .postFavorite(session, item.Id, 'DELETE')
+                          .then(status => {
+                            if (status === 200) {
+                              setIsFavorite(false);
+                            }
+                          });
+                      }}
                       style={styles.actionButton}>
                       <Text>
                         <Icon
@@ -202,18 +202,15 @@ const ListHeader = ({
                     </TouchableOpacity>
                   ) : (
                     <TouchableOpacity
-                      onPress={async () =>
+                      onPress={async () => {
                         await jellyfin
                           .postFavorite(session, item.Id, 'POST')
-                          .then(res => {
-                            mutate();
-                            console.log('Error posting favourites: ', res);
-                          })
-                          .catch(res => {
-                            mutate();
-                            console.log('Error posting favourites: ', res);
-                          })
-                      }
+                          .then(status => {
+                            if (status === 200) {
+                              setIsFavorite(true);
+                            }
+                          });
+                      }}
                       style={styles.actionButton}>
                       <Text>
                         <Icon

app/components/ContextMenu/ContextActionLike.tsx
@@ -17,10 +17,15 @@ const ContextActionLike = ({session, mediaItem}: ContextActionLikeProps) => {
   const [isFavorite, setIsFavorite] = useState<boolean>(false);
 
   useEffect(() => {
-    setIsFavorite(mediaItem.UserData.IsFavorite);
+    // Needed since the Trackplayer item and the MediaItem from Jellyfin
+    // aren't identical.
+    if (mediaItem.isFavorite !== undefined) {
+      setIsFavorite(mediaItem.isFavorite);
+    } else {
+      setIsFavorite(mediaItem.UserData.IsFavorite);
+    }
     setActionStatus('not triggered');
   }, []);
-
   return (
     <>
       {isFavorite ? (
@@ -29,7 +34,11 @@ const ContextActionLike = ({session, mediaItem}: ContextActionLikeProps) => {
           ioniconIcon="ios-heart"
           action={async () => {
             await jellyfin
-              .postFavorite(session, mediaItem.Id, 'DELETE')
+              .postFavorite(
+                session,
+                mediaItem.Id ? mediaItem.Id : mediaItem.id,
+                'DELETE',
+              )
               .then(status => {
                 if (status === 200) {
                   setActionStatus('success');
@@ -53,7 +62,11 @@ const ContextActionLike = ({session, mediaItem}: ContextActionLikeProps) => {
           ioniconIcon="ios-heart-outline"
           action={async () => {
             await jellyfin
-              .postFavorite(session, mediaItem.Id, 'POST')
+              .postFavorite(
+                session,
+                mediaItem.Id ? mediaItem.Id : mediaItem.id,
+                'POST',
+              )
               .then(status => {
                 if (status === 200) {
                   setIsFavorite(true);

app/screens/AlbumScreen.tsx
@@ -46,9 +46,6 @@ const AlbumScreen = ({navigation, route}: AlbumScreenProps) => {
 
   const playerState = usePlaybackState();
   const isPlaying = playerState === State.Playing;
-
-  console.log('Selected Storage', selectedStorageLocation);
-
   return (
     <>
       <View style={styles.container}>

commit baa0d026bb130bcbc4821c342cf3a030769e3694

Fix: Issue where clicking on a similar album would crash
The /Albums/${artistId}/Similar?limit=6&Fields=Genres endpoint doesn't
include the UserData object which would lead to boum crashing.
Regression from eddc9359e0a618edaebc2859e813e3f22c49f2e0

app/components/ListItems.tsx
@@ -72,7 +72,11 @@ const ListHeader = ({
 
   const [isFavorite, setIsFavorite] = useState<boolean>(false);
   useEffect(() => {
-    setIsFavorite(item.UserData.IsFavorite);
+    // Needed because the /Albums/${artistId}/Similar?limit=6&Fields=Genres
+    // endpoint doesn't include the UserData object.
+    if (item.UserData !== undefined) {
+      setIsFavorite(item.UserData.IsFavorite);
+    }
   }, []);
   return (
     <>

app/lib/api/jellyfinClient.tsx
@@ -1,7 +1,7 @@
 import useSWR from 'swr';
 
 import {versionBoum} from '@boum/constants';
-import {favoriteAction, Filter, Session, SortBy, SortOrder} from '@boum/types';
+import {favoriteAction, Session, SortBy, SortOrder} from '@boum/types';
 
 class jellyfinClient {
   private fetcher = (url: string, headers: string) =>