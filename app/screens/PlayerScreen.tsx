henniaufmrenni/boum

commit fc7613c0e59d2147a54c9bc43c7110b45109875e

Fix: Push new library screens on the navigation stack
Fixes #58

app/screens/PlayerScreen.tsx
@@ -86,7 +86,7 @@ const PlayerScreen = ({navigation}: PlayerScreenProps) => {
             <>
               <TouchableOpacity
                 onPress={() =>
-                  navigation.navigate('Album', {
+                  navigation.push('Album', {
                     itemId: currentTrack.albumId,
                     itemName: currentTrack.albumId,
                     item: undefined,
@@ -113,7 +113,7 @@ const PlayerScreen = ({navigation}: PlayerScreenProps) => {
               </TouchableOpacity>
               <TouchableOpacity
                 onPress={() =>
-                  navigation.navigate('Artist', {
+                  navigation.push('Artist', {
                     itemId: currentTrack.artistId,
                     itemName: currentTrack.artist,
                     item: undefined,

app/components/Artist/ArtistItemsListItem.tsx
@@ -18,7 +18,7 @@ class ArtistItemsRenderItem extends React.PureComponent {
       <View style={artistListItemStyle.container}>
         <TouchableOpacity
           onPress={() =>
-            this.props.navigation.navigate('Album', {
+            this.props.navigation.push('Album', {
               itemId: this.props.item.Id,
               name: this.props.item.Name,
               item: this.props.item,

app/components/ArtistComponents.tsx
@@ -53,7 +53,7 @@ class ArtistItemsRenderItem extends React.PureComponent {
         <TouchableOpacity
           title="Go to Album"
           onPress={() =>
-            this.props.navigation.navigate('Album', {
+            this.props.navigation.push('Album', {
               itemId: this.props.item.Id,
               name: this.props.item.Name,
               item: this.props.item,

app/components/Home/CustomHomeLists.tsx
@@ -27,7 +27,7 @@ const CustomHomeLists: React.FC<HeaderHomeProps> = ({navigation, data}) => {
         {data.map(item => (
           <TouchableOpacity
             onPress={() =>
-              navigation.navigate('List', {
+              navigation.push('List', {
                 listTitle: item.title,
                 sortBy: item.sortBy,
                 sortOrder: item.sortOrder,
@@ -45,7 +45,7 @@ const CustomHomeLists: React.FC<HeaderHomeProps> = ({navigation, data}) => {
           </TouchableOpacity>
         ))}
         {data.length === 0 ? (
-          <TouchableOpacity onPress={() => navigation.navigate('ListManager')}>
+          <TouchableOpacity onPress={() => navigation.push('ListManager')}>
             <View style={styles.itemContainer}>
               <Text style={styles.text}>
                 <Icon name={'add'} size={20} />

app/components/Library/AlbumCard.tsx
@@ -28,7 +28,7 @@ class AlbumCard extends React.PureComponent<AlbumCardProps> {
       <View style={listRowStyles.container}>
         <TouchableOpacity
           onPress={() =>
-            this.props.navigation.navigate(this.props.navigationDestination, {
+            this.props.navigation.push(this.props.navigationDestination, {
               itemId: this.props.item.Id,
               name: this.props.item.Name,
               item: this.props.item,

app/components/Library/LibraryListItem.tsx
@@ -18,7 +18,7 @@ class LibraryListItem extends React.PureComponent<LibraryListItemProps> {
     return (
       <TouchableHighlight
         onPress={() =>
-          this.props.navigation.navigate(this.props.navigationDestination, {
+          this.props.navigation.push(this.props.navigationDestination, {
             itemId: this.props.item.Id,
             name: this.props.item.Name,
             item: this.props.item,

app/components/ListItems.tsx
@@ -30,7 +30,7 @@ import {
   Session,
 } from '@boum/types';
 
-import {ArtistItems} from './ArtistComponents';
+import {ArtistItems} from '@boum/components/ArtistComponents';
 import TrackPlayer from 'react-native-track-player';
 import {SlideInContextMenu} from '@boum/components/ContextMenu';
 import {jellyfinClient} from '@boum/lib/api';
@@ -119,7 +119,7 @@ const ListHeader = ({
             {item.AlbumArtists ? (
               <TouchableOpacity
                 onPress={() =>
-                  navigation.navigate('Artist', {
+                  navigation.push('Artist', {
                     itemId: item.AlbumArtists[0].Id,
                     itemName: item.AlbumArtists[0].Name,
                     item: undefined,

app/components/Lists/AlbumCard.tsx
@@ -28,7 +28,7 @@ class AlbumCard extends React.PureComponent<AlbumCardProps> {
       <View style={listRowStyles.container}>
         <TouchableOpacity
           onPress={() =>
-            this.props.navigation.navigate(this.props.navigationDestination, {
+            this.props.navigation.push(this.props.navigationDestination, {
               itemId: this.props.item.Id,
               name: this.props.item.Name,
               item: this.props.item,

commit 2a42c32eb7a2ad4b53ae9045d5f38196079600f0

Fix: Navigation from Player to Album and Artist
Turns out we need to call navigation.navigate() and not navigation.push()
if we're trying to navigate from another stack.
Fixes a regression introduced in fc7613c0e59d2147a54c9bc43c7110b45109875e

app/screens/PlayerScreen.tsx
@@ -86,7 +86,7 @@ const PlayerScreen = ({navigation}: PlayerScreenProps) => {
             <>
               <TouchableOpacity
                 onPress={() =>
-                  navigation.push('Album', {
+                  navigation.navigate('Album', {
                     itemId: currentTrack.albumId,
                     itemName: currentTrack.albumId,
                     item: undefined,
@@ -113,7 +113,7 @@ const PlayerScreen = ({navigation}: PlayerScreenProps) => {
               </TouchableOpacity>
               <TouchableOpacity
                 onPress={() =>
-                  navigation.push('Artist', {
+                  navigation.navigate('Artist', {
                     itemId: currentTrack.artistId,
                     itemName: currentTrack.artist,
                     item: undefined,